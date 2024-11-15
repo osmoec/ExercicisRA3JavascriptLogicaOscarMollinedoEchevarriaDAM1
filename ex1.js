d307cc714a76d78d9106381fd5281e1a091e365c
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-11-15 08:35:59 +0100

    ex1

---
+++ ex1.js
@@ -0,0 +1,13 @@
+//ex1 Decideix si un número és múltiple de 3 o de 7
+
+var num = 14
+
+if ((num % 3) === 0){
+ console.log(num+" es multiple de 3")
+}
+else if ((num % 7) === 0){
+    console.log(num+" es multiple de 7")
+}
+else{
+    console.log("no es multiple de 3 o 7")
+}
