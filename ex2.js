d974f96aa1e515971b41fa136707909761d2f9a5
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-11-18 10:46:06 +0100

    ex2

---
+++ ex2.js
@@ -0,0 +1,8 @@
+//ex2 Donats dos enters (x, y), dir quins d’ells estan en el rang de 10 a 50 inclosos. Utilitza un
+// operador ternari.
+
+var num1 = 2
+
+var num2 = 48
+
+num1 > 10 && num1 < 50 && num2 > 10 && num2 < 50 ? console.log("entren al rang") : num1 > 10 && num1 < 50 ? console.log("el numero 1 entra al rang") :  num2 > 10 && num2 < 50 ? console.log("el numero 2 entra al rang") : console.log("cap dels dos entra al rang")
